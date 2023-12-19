f20cfb21ac96ff03138e20a6027e4f4962a408dc
kumarnivi <nivethikashivakumar56@gmail.com>
2023-12-19 18:06:33 +0530

    build file4

--- Backend/DB/database.js
+++ Backend/DB/database.js
@@ -1,16 +1,16 @@
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mydb', 'root', 'root0', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-
-// const sequelize = new Sequelize('bpuimhpednzk8ebzbzxg', 'uxwbhkqhrhocxx1h', 'NXxYdz7ZXbCNhy1R6oQo', {
-//   host: 'bpuimhpednzk8ebzbzxg-mysql.services.clever-cloud.com',
+// const sequelize = new Sequelize('mydb', 'root', 'root0', {
+//   host: 'localhost',
 //   dialect: 'mysql'
 // });
+
+
+const sequelize = new Sequelize('bpuimhpednzk8ebzbzxg', 'uxwbhkqhrhocxx1h', 'NXxYdz7ZXbCNhy1R6oQo', {
+  host: 'bpuimhpednzk8ebzbzxg-mysql.services.clever-cloud.com',
+  dialect: 'mysql'
+});
 
 
 // Test the connection
